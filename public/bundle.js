6c83157a0b3e4f8797f8b265dd818f12d97aeb14
Rub21 <ruben@mapbox.com>
2015-08-08 13:41:10 -0500

    final canvas

--- public/bundle.js
+++ public/bundle.js
@@ -15864,10 +15864,15 @@ var _ = require('underscore');
 var help = require('./helper/measures');
 
 var record = document.querySelector('#butto_imagen');
+var download = document.querySelector('#download_imagen');
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 var serv = ['a', 'b', 'c'];
+var wh_tiles = [];
+var wh_poly = [];
+var wh_nim_poly = [];
+
 
 record.addEventListener('click', function(e) {
 	e.preventDefault();
@@ -15914,24 +15919,21 @@ record.addEventListener('click', function(e) {
 	canvas.height = 256 * vertical.length;
 	con(canvas.width + '-X-' + canvas.height);
 
-	// var wh_tiles = [];
-	// wh_tiles.push(canvas.width);
-	// wh_tiles.push(canvas.height);
+	wh_tiles.push(canvas.width);
+	wh_tiles.push(canvas.height);
 
-	// var wh_poly = [];
-	// wh_poly.push(canvas.width / size_tiles[0] * size_poly[0]);
-	// wh_poly.push(canvas.height / size_tiles[1] * size_poly[1]);
+	wh_poly.push(canvas.width / size_tiles[0] * size_poly[0]);
+	wh_poly.push(canvas.height / size_tiles[1] * size_poly[1]);
 
-	// var wh_nim_poly = [];
-	// wh_nim_poly.push(canvas.width / size_tiles[0] * size_nim_poly[0]);
-	// wh_nim_poly.push(canvas.height / size_tiles[1] * size_nim_poly[1]);
+	wh_nim_poly.push(canvas.width / size_tiles[0] * size_nim_poly[0]);
+	wh_nim_poly.push(canvas.height / size_tiles[1] * size_nim_poly[1]);
 
-	// con(size_tiles)
-	// con(wh_tiles.join('X'));
-	// con(size_poly)
-	// con(wh_poly.join('X'));
-	// con(size_nim_poly)
-	// con(wh_nim_poly.join('X'));
+	con(size_tiles)
+	con(wh_tiles.join('X'));
+	con(size_poly)
+	con(wh_poly.join('X'));
+	con(size_nim_poly)
+	con(wh_nim_poly.join('X'));
 
 	for (var i = 0; i < vertical.length; i++) {
 		for (var j = 0; j < horizontal.length; j++) {
@@ -15939,11 +15941,27 @@ record.addEventListener('click', function(e) {
 			con(url)
 			var x = 256 * j;
 			var y = 256 * i;
-			loadImage(url, x, y);
+			var islast = false;
+			//add imagen to download
+			if ((i === vertical.length - 1) && (j === horizontal.length - 1)) {
+				islast = true;
+			}
+			loadImage(url, x, y, islast);
 		};
 	};
-	//}, 100);
+
+
 }, false);
+
+
+
+// download.addEventListener('click', function(e) {
+
+// 	downloadCanvas('test.png');
+
+// }, false);
+
+
 
 function logError(err) {
 	console.error(err);
@@ -15953,17 +15971,68 @@ function con(s) {
 	console.log(s);
 }
 
-function loadImage(url, x, y) {
+function loadImage(url, x, y, islast) {
 	var image = new Image();
+	image.setAttribute('crossOrigin', 'anonymous');
 	image.src = url;
 	image.onload = function() {
 		ctx.drawImage(image, x, y);
+
+		if (islast) {
+
+			// var wh_tiles = [];
+			// var wh_poly = [];
+			// var wh_nim_poly = [];
+			//console.log(img)
+			var img = new Image();
+			canvas_result
+			var canvas_result = document.getElementById("canvas");
+			var ctx_result = canvas_result.getContext("2d");
+
+
+			img.src = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+			console.log(img)
+
+			img.onload = function() {
+
+				ctx_result.drawImage(img, wh_tiles[0], wh_tiles[1], wh_nim_poly[0], wh_nim_poly[1], 0, 0, wh_poly[0], wh_poly[1]);
+				download.href = img.src //.replace("image/png", "image/octet-stream");
+				console.log(download.href)
+				download.download = 'test.png';
+
+				console.log(download)
+
+			};
+
+
+
+			// setTimeout(function() {
+
+			// 	imageObj.onload = function() {
+
+			// 		download.href = canvas.toDataURL("image/png")
+			// 	};
+
+			// }, 3000);
+
+		}
 	};
+
+
 }
 
 function ale() {
 	minimum = 0;
 	maximum = 2;
 	return Math.round(Math.random() * (maximum - minimum) + minimum);
+}
+
+
+function downloadCanvas(filename) {
+	var link = document.getElementById('download');
+	link.href = document.getElementById('canvas').toDataURL("image/png"); //.replace("image/png", "image/octet-stream");
+	console.log(link.href)
+	link.download = filename;
 }
 },{"./helper/measures":1,"tile-cover":6,"turf":8,"underscore":131}]},{},[132]);
